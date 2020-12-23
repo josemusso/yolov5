b10bb214c0eb81b7816b473cb01a8cb6c9f03d20
Jose Ignacio Musso <jmusso@live.com>
2020-12-23 11:23:52 -0300

    fix ds

--- detect.py
+++ detect.py
@@ -233,6 +233,7 @@ if __name__ == '__main__':
     parser.add_argument('--project', default='runs/detect', help='save results to project/name')
     parser.add_argument('--name', default='exp', help='save results to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument("--config_deepsort", type=str, default="deep_sort_pytorch/configs/deep_sort.yaml")
     opt = parser.parse_args()
     print(opt)
 
